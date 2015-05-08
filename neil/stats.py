e7e84545d64f061ca52390e1837601f87067802d
nkmunjal <nkmunjal@gmail.com>
2015-05-08 15:02:39 -0400

    stats.py can dump all patients to a CSV with their nodal stats

--- neil/stats.py
+++ neil/stats.py
@@ -4,6 +4,7 @@ from scipy import stats
 import numpy as np
 import bct
 import warnings
+import pylab
 
 control_folder = corr.control_folder
 control = corr.control
@@ -11,6 +12,7 @@ population_folder = corr.population_folder
 population = corr.population
 filename = 'corr_rois_pearson_new_r.txt'
 
+names = [control,population]
 everyone = [] # [0] is control supermatrix, [1] is population; format: [n][x][y][i]
 everyone_mask = []
 mat,mask = corr.load_patients([control_folder+'/'+i+'/'+filename for i in control])
@@ -24,9 +26,9 @@ everyone_mask.append(mask)
 everyone_mapped = [] # WARNING: changes format from everyone; [n][i][x][y] where n is population as above, i is pt, x,y are adjacency matrix
 percentile,beta = -2,-2
 # comment out the undesired methods
-map_technique,percentile = corr.HARD,0.9
+#map_technique,percentile = corr.HARD,0.9
 #map_technique,percentile = corr.HARD_WEIGHTED,0.9
-#map_technique,beta = corr.SOFT,12
+map_technique,beta = corr.SOFT,12
 weighted = False if map_technique == corr.HARD else True
 for i in range(0,len(everyone)): # pick group (control vs pop)
     pop = []
@@ -35,7 +37,7 @@ for i in range(0,len(everyone)): # pick group (control vs pop)
     everyone_mapped.append(pop)
 
 # generate stats
-everyone_stats = []
+everyone_stats = [] # [n][dict]
 for i in range(0,len(everyone_mapped)):
     pop_stats = []
     for j in range(0,len(everyone_mapped[i])):
@@ -55,10 +57,23 @@ for k,v in everyone_stats[0][0].iteritems():
 global_measures.remove("num_edges")
 global_measures.remove("num_vertices")
 global_measures.remove("mean_degree")
+global_measures.remove("assortativity_binary")
+global_measures.remove("mean_assortativity_binary")
+global_measures.remove("assortativity_weighted")
+global_measures.remove("mean_assortativity_weighted")
 local_measures.remove("eccentricity")
+local_measures.remove("community_structure")
+if map_technique==corr.SOFT:
+    global_measures.remove("global_efficiency")
+    global_measures.remove("giant_component")
+    global_measures.remove("diameter")
+    global_measures.remove("radius")
+    global_measures.remove("charpath")
+    local_measures.remove("degree")
+    local_measures.remove("betweenness_binary")
 
 # remainder of code assumes that everyone.* arrays have only 2 entries, control = 0, pop = 1
-
+"""
 # compare global measures
 print "{:30s} {:>10s}{:>10s}{:>10s}{:>10s}{:>10s}{:>10s}".format("key","mean_c","mean_p","std_c","std_p","t-score","p-value")
 for key in global_measures:
@@ -73,7 +88,7 @@ read='ROI_nodes_new.node'
 roi_list = corr.get_ROI_list(read)
 roi_index = [mni.find_closest(mni.common_roi[i],roi_list)[0] for i in rois]
 
-for i in range(0,len(rois)):
+for i in range(0,len(roi_index)):
     roi = rois[i]
     ind = roi_index[i]
     print ""
@@ -84,3 +99,29 @@ for i in range(0,len(rois)):
         population = [j[key][ind] for j in everyone_stats[1]]
         s = stats.ttest_ind(control,population)
         print "{:30s} {:10.4f}{:10.4f}{:10.4f}{:10.4f}{:10.4f}{:10.4f}".format(key[:30], np.mean(control),np.mean(population),np.std(control),np.std(population),s[0],s[1])
+
+"""
+all_measures = local_measures
+out_folder = '/Volumes/Serena/SPECC/Neil/bpd_rest/neil/stats_output/'
+# create file for each subject, rows are ROIs, columns are statistical measures
+for i in range(0,len(everyone_stats)):
+    for j in range(0,len(everyone_stats[i])):
+        out_filename = names[i][j]
+        f = open(out_folder+out_filename,'w')
+        f.write("ROI,"+",".join(all_measures)+'\n')
+        for k in range(0,len(everyone_stats[i][j][all_measures[0]])):
+            f.write(str(k)+','+','.join([str(everyone_stats[i][j][measure][k]) for measure in all_measures])+'\n')
+        print out_filename
+        f.close()
+            
+
+
+"""
+roi = 0
+measures = ["strength","local_efficiency","clustering_weighted","pagerank","betweenness_binary","eigenvector_centrality_und"]
+for i in range(0,len(measures)):
+    pylab.subplot(2,3,i+1)
+    pylab.title(measures[i])
+    pylab.hist([j[measures[i]][roi_index[roi]] for j in everyone_stats[0]],20)
+pylab.show()
+"""
