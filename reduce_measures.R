07bc062cadbb360f3577e51a16c06f33c2857980
nkmunjal <nkmunjal@gmail.com>
2015-05-19 17:34:58 -0400

    reduce_measures: finished PCA (though normalization still needed) and basic non-parametric testing using permutation test

--- reduce_measures.R
+++ reduce_measures.R
@@ -44,26 +44,50 @@ pattype = ifelse(allsubjs$id > "0" & allsubjs$id < "1",0,1) # 0 = patient, 1 = c
 dataset <- cbind(allsubjs, f1$scores,pattype)
 
 components = colnames(f1$scores)
-colmeans_control = colMeans(dataset[dataset$pattype==1,components])
-colmeans_pat = colMeans(dataset[dataset$pattype==0,components])
-diff.observed = colmeans_pat-colmeans_control
 
-len_p = length(dataset[dataset$pattype==0,components[1]])
-len_c = length(dataset[dataset$pattype==1,components[1]])
+permutation.test <- function(data,components,num_permutations = 1000) {
+	# assumes separator variable is pattype
+	colmeans_control = colMeans(data[data$pattype==1,components])
+	colmeans_pat = colMeans(data[data$pattype==0,components])
+	diff.observed = colmeans_pat-colmeans_control
 
-num_permutations = 1000
-diff.random = matrix(,num_permutations,length(components))
-for (i in 1 : num_permutations){
-	for (j in 1 : length(components))
-	{
-		shuffled = sample(dataset[,components[j]],length(dataset[,components[j]]))
-		#a.random = shuffled[1:len_c]
-		#b.random = shuffled[(len_c)+1:length(dataset[,components[j]])]
-		a.random = head(shuffled,len_c)
-		b.random = tail(shuffled,len_p)
-		diff.random[i,j] = mean(a.random) - mean(b.random)
+	len_p = length(data[data$pattype==0,components[1]])
+	len_c = length(data[data$pattype==1,components[1]])
+
+	num_permutations = 1000
+	diff.random = matrix(,num_permutations,length(components))
+	for (i in 1 : num_permutations){
+		for (j in 1 : length(components))
+		{
+			shuffled = sample(data[,components[j]],length(data[,components[j]]))
+			#a.random = shuffled[1:len_c]
+			#b.random = shuffled[(len_c+1):length(data[,components[j]])]
+			a.random = head(shuffled,len_c)
+			b.random = tail(shuffled,len_p)
+			diff.random[i,j] = mean(a.random) - mean(b.random)
+		}
 	}
+	pvalue = colSums(abs(diff.random) >= abs(diff.observed)) / num_permutations
+	return(pvalue)
 }
 
-pvalue = colSums(abs(diff.random) >= abs(diff.observed)) / num_permutations
-print (pvalue)
+all_rois = unique(dataset$roi)
+values = matrix(,length(all_rois),length(components))
+colnames(values) = components
+for (i in 1:length(all_rois)){
+	data = dataset[dataset$roi == all_rois[i],]
+	values[i,]=(permutation.test(data,components))
+}
+values = cbind(all_rois,values)
+values[values[,'PC1']<0.05 | values[,'PC2'] < 0.05 | values[,'PC3'] < 0.05,]
+
+graph.hist <- function(one,two,breaks=10) {
+	p1 = hist(one,breaks=breaks,plot=FALSE)
+	p2 = hist(two,breaks=breaks,plot=FALSE)
+	plot(p1,col=rgb(0,0,1,1/4))
+	plot(p2,col=rgb(1,0,0,1/4),add=T)
+}
+
+graph.hist(dataset[dataset$pattype==1,'PC1'],dataset[dataset$pattype==0,'PC2'],breaks=100)
+graph.hist(dataset[dataset$pattype==1 & dataset$roi == 1,'PC1'],dataset[dataset$pattype==0 & dataset$roi == 1,'PC2'],breaks=10)
+
