2cd0666638f1c98721561c2ef313e27ef71dbf14
Nathan Hall <natehall@Nathans-MacBook-Pro.local>
2017-05-27 16:42:03 -0400

    new file:   Nate/10nodes_scotmi_pearson.pdf 	modified:   Nate/calcGraph_binary.R

--- Nate/calcGraph_binary.R
+++ Nate/calcGraph_binary.R
@@ -1,9 +1,10 @@
-####worker bee function which compiles nodal graph metrics including nodal metrics with reference to an input community structure
+####worker bee function which compiles nodal graph metrics including nodal metrics with reference to a $community attribute of the graph 
 calcGraph_binary_nodal <- function(origgraph) {
   edgelist <- data.frame(get.edgelist(origgraph))
-  origgraph <- tagGraph(origgraph, nodefile="~/Box Sync/RS_BPD_graph/power269_masterlookup_precull.csv", atlasname="power269")
+  origgraph <- tagGraph(origgraph, nodefile="~/Box Sync/RS_BPD_graph/power269_masterlookup_shift_nate.csv", atlasname="power269")
   allmetrics <- list()
   
+  ##centrality
   allmetrics[["origgraph"]] <- origgraph
   allmetrics[["local.clustering"]] <- transitivity(origgraph, type = "local")
   allmetrics[["degree"]] <- degree(origgraph, v = V(origgraph))
@@ -15,8 +16,13 @@ calcGraph_binary_nodal <- function(origgraph) {
 
   allmetrics[["community.membership"]] <- V(origgraph)$community
  
-  allmetrics[["within.module.deg.zscore"]] <- within_module_deg_z_score(origgraph, V(origgraph)$community, use.parallel=FALSE)
-  allmetrics[["part.coeff"]] <- part.coeff(origgraph, V(origgraph)$community, use.parallel = FALSE)
+  ##nodal within vs between modules
+  wibw <- wibw_module_degree(origgraph)
+  allmetrics[["within.module.deg.zscore"]] <- wibw$z_within
+  allmetrics[["between.module.deg.zscore"]] <- wibw$z_between
+  allmetrics[["part.coeff"]] <- part_coeff(origgraph, V(origgraph)$community)#, use.parallel = FALSE)
+  allmetrics[["gateway.coeff.btw"]] <- gateway_coeff(origgraph, V(origgraph)$community, centr = "btwn.cent")
+  allmetrics[["gateway.coeff.deg"]] <- gateway_coeff(origgraph, V(origgraph)$community, centr = "degree")
 
   return(allmetrics)
 }
@@ -24,15 +30,14 @@ calcGraph_binary_nodal <- function(origgraph) {
 ####worker bee function which compiles global graph theoretical metrics
 calcGraph_binary_global <- function(origgraph) {
   edgelist <- data.frame(get.edgelist(origgraph))
-  origgraph <- tagGraph(origgraph, nodefile="~/Box Sync/RS_BPD_graph/power269_masterlookup_precull.csv", atlasname="power269")
+  origgraph <- tagGraph(origgraph, nodefile="~/Box Sync/RS_BPD_graph/power269_masterlookup_shift_nate.csv", atlasname="power269")
   globmetrics <- list()
 
   globmetrics[["origgraph"]] <- origgraph
-  #globmetrics[["shortest_path"]] <- shortest.paths(origgraph)
   globmetrics[["characteristic_path_length"]] <- mean_distance(origgraph)
-  #globmetrics[["number_triangles"]] <- count_triangles(origgraph)
   globmetrics[["clustering_coefficient"]] <- transitivity(origgraph, type = "global")
-  #globmetrics[["small_worldness"]] <- small.world(origgraph)
+  globmetrics[["small_worldness"]] <- (mean_distance(origgraph)/mean_distance(rewire(origgraph, with = keeping_degseq(loops = FALSE, niter = 1000))))/(transitivity(origgraph, type = "global")/transitivity(rewire(origgraph, with = keeping_degseq(loops = FALSE, niter = 1000)), type = "global"))
+  globmetrics[["modularity"]] <- modularity(origgraph, vertex_attr(origgraph)$community)
   
   return(globmetrics)
 }
